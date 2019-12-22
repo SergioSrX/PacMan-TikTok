e7ade5d4c1611005daffe0cdb6962cf6dba51c11
Michael Kazda <michaelkazda@seznam.cz>
2019-12-23 00:55:14 +0100

    Trying to get user data, but tiktok is broken and it doesnt fkin work

--- TestFiles/get_data_user_CZECH.py
+++ TestFiles/get_data_user_CZECH.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import scrapy
+
+class get_data_user(scrapy.Spider):
+    name = 'userInfo'
+    start_urls = ["https://www.tiktok.com/@realmadrid?lang=en"] # Todo - find out how to parse from this link, scrapy not returning html but json
+
+    def parse(self, response):
+        yield{
+            'userName': response.css('h1.share-title::text'),
+            'userTag': response.css('h1.share-sub-title::text'),
+            #'userVerified': response.css('span.jsx-874583638').__bool__(),
+            'userDescription': response.css('h2.share-desc::text'),
+        }
