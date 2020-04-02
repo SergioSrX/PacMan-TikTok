97b4659e8498dbef09fb7de193c4a1474d8fac69
Michael Kazda <michaelkazda@seznam.cz>
2020-04-02 19:41:13 +0200

    Fix crawelr script
    TikRank.com has changed its link structure and also some data that provides, that caused bugs. FIXED

--- FinalFiles/GetData/get_data_user.py
+++ FinalFiles/GetData/get_data_user.py
@@ -6,22 +6,29 @@ import json
 class get_data_user(scrapy.Spider):
     # Spider setup variables
     name = 'userInfo'
-    custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}  # Modifying crawler's options (USER_AGENT), so crawler doens't behave like a bot and it can get response from page
+    custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}  # Modifying crawler's options (USER_AGENT), so crawler doens't behave like a bot and it can get response from page
 
     # Setting user links
     start_urls = []
-    countries = ['ES', 'US', 'IN', 'JP', 'KH', 'TH', 'GB', 'FR', 'RU', 'DE']  # List of countries from which we get TOP users (1000 users)
+    countries = {'Spain':'ES', 'USA':'US', 'India':'IN', 'Japan':'JP', 'Cambodia':'KH', 'Thailand':'TH', 'United Kingdom':'GB', 'France':'FR', 'Russian Federation':'RU', 'Germany':'DE'}  # Dictionary of countries from which we get TOP 100 users (1000 users)
     for country in countries:
-        start_urls.append("https://tikrank.com/analysis/rank?region={}&count=100".format(country))  # link for getting user link - region (US, IN, empty[world]), count (50 or 100)
+        start_urls.append("https://tikrank.com/influencer/influencers?page_size=100&country={}".format(countries[country]))  # link for getting user link - region (US, IN, empty[world]), count (50 or 100)
     userLinkBefore = "https://www.tiktok.com/@"
 
     def parse(self, response):
-        jsonDataIn = json.loads(response.text)  # getting user profile links and other data from tikrank.com
+        # getting user region from request url
+        userRegShort = response.request.url.split("=")[2]
+        for regLong, regShort in self.countries.items():
+            if userRegShort == regShort:
+                userRegLong = regLong
+                break
+
+        # getting user profile links and other data from tikrank.com
+        jsonDataIn = json.loads(response.text)
         x = 0
-        for item in jsonDataIn['data']:
+        for item in jsonDataIn['data']['kols']:
             x += 1
-            yield scrapy.Request(self.userLinkBefore + item['platform_unique_id'], callback=self.get_profile_data,meta={'itemNum': x, 'userLink': self.userLinkBefore + item['platform_unique_id'],'userRegion': item['region_str']})  # getting data from tiktok.com based on received profile links and passing some meta data
+            yield scrapy.Request(self.userLinkBefore + item['kol_unique_id'], callback=self.get_profile_data, meta={'itemNum': x, 'userLink': self.userLinkBefore + item['kol_unique_id'], 'userRegion': userRegLong})  # getting data from tiktok.com based on received profile links and passing some meta data
 
     # Getting user's profile data
     def get_profile_data(self, response):
@@ -40,7 +47,6 @@ class get_data_user(scrapy.Spider):
                         'userLikes': rawUserJsonData['stats']['heartCount'],
                         'userVideosCount': rawUserJsonData['stats']['videoCount']
                         }
-
             # MongoDB setup variables
             uri = 'mongodb+srv://packman:MIB123456@packman-mib-wil2x.azure.mongodb.net/test?retryWrites=true&w=majority'
             client = MongoClient(uri)
