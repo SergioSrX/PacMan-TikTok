1359da0c3454a29029c160c6e5edaa70195614bf
Michael Kazda <michaelkazda@seznam.cz>
2020-04-25 23:36:56 +0200

    Adding datetime to user_stats data

--- FinalFiles/GetData/get_data_user.py
+++ FinalFiles/GetData/get_data_user.py
@@ -2,15 +2,17 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from pymongo import MongoClient
 import json
+from time import sleep as pause
+from datetime import datetime as time
 
 class get_data_user(scrapy.Spider):
     # Spider setup variables
     name = 'userInfo'
-    custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}  # Modifying crawler's options (USER_AGENT), so crawler doens't behave like a bot and it can get response from page
+    custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}  # Modifying crawler's options (USER_AGENT), so crawler doens't behave like a bot and it can get response from page
 
     # Setting user links
     start_urls = []
-    countries = {'Spain':'ES', 'USA':'US', 'India':'IN', 'Japan':'JP', 'Cambodia':'KH', 'Thailand':'TH', 'United Kingdom':'GB', 'France':'FR', 'Russian Federation':'RU', 'Germany':'DE'}  # Dictionary of countries from which we get TOP 100 users (1000 users)
+    countries = {'Spain':'ES','USA':'US', 'India':'IN', 'Japan':'JP', 'Cambodia':'KH', 'Thailand':'TH', 'United Kingdom':'GB', 'France':'FR', 'Russian Federation':'RU', 'Germany':'DE'}  # Dictionary of countries from which we get TOP 100 users (1000 users)
     for country in countries:
         start_urls.append("https://tikrank.com/influencer/influencers?page_size=100&country={}".format(countries[country]))  # link for getting user link - region (US, IN, empty[world]), count (50 or 100)
     userLinkBefore = "https://www.tiktok.com/@"
@@ -30,6 +32,7 @@ class get_data_user(scrapy.Spider):
 
     # Getting user's profile data
     def get_profile_data(self, response):
+        pause(0.5)  # 0.5sec pause so we dont get timeouted from Tiktok server
         try:
             rawUserJsonData = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userInfo']
             userInfo = {'itemNum': response.meta['itemNum'],
@@ -40,20 +43,28 @@ class get_data_user(scrapy.Spider):
                         'userRegion': response.meta['userRegion'],
                         'userImg': rawUserJsonData['user']['avatarMedium'],
                         'userVerified': rawUserJsonData['user']['verified'],
-                        'userFollowing': rawUserJsonData['stats']['followingCount'],
-                        'userFollowers': rawUserJsonData['stats']['followerCount'],
-                        'userLikes': rawUserJsonData['stats']['heartCount'],
-                        'userVideosCount': rawUserJsonData['stats']['videoCount']
                         }
+
             # MongoDB setup variables
             uri = 'mongodb+srv://packman:MIB123456@packman-mib-wil2x.azure.mongodb.net/test?retryWrites=true&w=majority'
             client = MongoClient(uri)
             db = client.userData
-            db.userData.update_one({'itemNum': userInfo['itemNum']}, {'$set': userInfo}, True)  # Updates existing or inserts new one
+            db.userData.update_one({'itemNum': userInfo['itemNum']},
+                                   {'$set': userInfo,
+                                   '$push':
+                                        {'userStats':
+                                             {'datetime': time.utcnow().strftime("%d-%m-%Y %X"),
+                                              'userFollowing': rawUserJsonData['stats']['followingCount'],
+                                              'userFollowers': rawUserJsonData['stats']['followerCount'],
+                                              'userLikes': rawUserJsonData['stats']['heartCount'],
+                                              'userVideosCount': rawUserJsonData['stats']['videoCount']
+                                            }
+                                        }
+                                    }, True)  # Updates existing or inserts new one
         except():
             print("Not able the get info about this user:" + response.meta['userLink'])
 
-# code for runnig spider straight from script and not thru cmd
+# code for runnig spider straight from script and not with CLI
 process = CrawlerProcess(settings={
     'FEED_FORMAT': 'json',
     'FEED_URI': 'items.json'
