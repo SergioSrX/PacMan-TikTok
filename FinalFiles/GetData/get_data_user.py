2d50dcd080b7021edc71d48c15667d0eeeb4e769
Michael Kazda <michaelkazda@seznam.cz>
2020-04-29 15:00:21 +0200

    Error handling

--- FinalFiles/GetData/get_data_user.py
+++ FinalFiles/GetData/get_data_user.py
@@ -1,6 +1,7 @@
 import json
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.spidermiddlewares.httperror import HttpError
 from pymongo import MongoClient
 from time import sleep as pause
 from datetime import datetime as time
@@ -20,7 +21,7 @@ class get_data_user(scrapy.Spider):
     start_urls = []
     countries = {'Spain':'ES','USA':'US', 'India':'IN', 'Japan':'JP', 'Cambodia':'KH', 'Thailand':'TH', 'United Kingdom':'GB', 'France':'FR', 'Russian Federation':'RU', 'Germany':'DE'}  # Dictionary of countries from which we get TOP 100 users (1000 users)
     for country in countries:
-        start_urls.append("https://tikrank.com/influencer/influencers?page_size=100&country={}".format(countries[country]))  # link for getting user link - region (US, IN, empty[world]), count (50 or 100)
+        start_urls.append("https://tikrank.com/influencer/influencers?page_size=1&country={}".format(countries[country]))  # link for getting user link - region (US, IN, empty[world]), count (50 or 100)
     userLinkBefore = "https://www.tiktok.com/@"
 
     # Method for getting user profile links from Tiktank.com
@@ -36,46 +37,48 @@ class get_data_user(scrapy.Spider):
         jsonDataIn = json.loads(response.text)
         for item in jsonDataIn['data']['kols']:
             # Calling method get_profile_data and passing some meta data
-            yield scrapy.Request(self.userLinkBefore + item['kol_unique_id'], callback=self.get_profile_data, meta={'userLink': self.userLinkBefore + item['kol_unique_id'], 'userRegion': userRegLong})
+            yield scrapy.Request(self.userLinkBefore + item['kol_unique_id'], callback=self.get_profile_data, meta={'userLink': self.userLinkBefore + item['kol_unique_id'], 'userRegion': userRegLong}, errback=self.get_profile_data_err_handler)
+
+    # Method for handling errors of scraping user's profile data
+    def get_profile_data_err_handler(self, failure):
+        if failure.check(HttpError):
+            self.logger.error('Not able to get data from this user Url: ' + failure.value.response.url)
 
     # Method for getting user's profile data
     def get_profile_data(self, response):
-        pause(0.5)  # 0.5sec pause so we dont get timeouted from Tiktok server
-        try:
-            rawUserData = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userInfo']
-            userInfo = {'userID': rawUserData['user']['id'],
-                        'userSecUid': rawUserData['user']['secUid'],
-                        'userName': rawUserData['user']['nickname'],
-                        'userTag': rawUserData['user']['uniqueId'],
-                        'userLink': response.meta['userLink'],
-                        'userDescription': rawUserData['user']['signature'],
-                        'userRegion': response.meta['userRegion'],
-                        'userImg': rawUserData['user']['avatarMedium'],
-                        'userVerified': rawUserData['user']['verified'],
-                        }
+        pause(0.5)  # 0.5sec pause so we dont get timeout on Tiktok server
+        rawUserData = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userInfo']
+        userInfo = {'userID': rawUserData['user']['id'],
+                    'userSecUid': rawUserData['user']['secUid'],
+                    'userName': rawUserData['user']['nickname'],
+                    'userTag': rawUserData['user']['uniqueId'],
+                    'userLink': response.meta['userLink'],
+                    'userDescription': rawUserData['user']['signature'],
+                    'userRegion': response.meta['userRegion'],
+                    'userImg': rawUserData['user']['avatarMedium'],
+                    'userVerified': rawUserData['user']['verified'],
+                    }
 
-            # Updates existing user data or inserts new one
-            self.db.userData.update_one({'userID': userInfo['userID']},
-                                   {'$set': userInfo,
-                                   '$push':
-                                        {'userStats':
-                                             {'datetime': time.utcnow().strftime("%d-%m-%Y %X"),
-                                              'userFollowing': rawUserData['stats']['followingCount'],
-                                              'userFollowers': rawUserData['stats']['followerCount'],
-                                              'userLikes': rawUserData['stats']['heartCount'],
-                                              'userVideosCount': rawUserData['stats']['videoCount']
-                                            }
+        # Updates existing user data or inserts new one
+        self.db.userData.update_one({'userID': userInfo['userID']},
+                               {'$set': userInfo,
+                               '$push':
+                                    {'userStats':
+                                         {'datetime': time.utcnow().strftime("%d-%m-%Y %X"),
+                                          'userFollowing': rawUserData['stats']['followingCount'],
+                                          'userFollowers': rawUserData['stats']['followerCount'],
+                                          'userLikes': rawUserData['stats']['heartCount'],
+                                          'userVideosCount': rawUserData['stats']['videoCount']
                                         }
-                                    }, True)
+                                    }
+                                }, True)
 
-            # Generating user media singature using nodeJS browser.js script. Script is called using Naked lib
-            userMediaLink = "https://m.tiktok.com/api/item_list/?count=30&id=" + userInfo['userID'] + "&type=1&secUid=" + userInfo['userSecUid'] + "&maxCursor=0&minCursor=0&sourceType=8&appId=1233&region=CZ&language=cs&verifyFp="
-            userMediaSignature = muterun_js('D:\\NodeJS\\node_modules\\tiktok-signature\\browser.js', '"' + userMediaLink + '"')   # TODO get correct absolute path to the script
-            if userMediaSignature.exitcode == 0:
-                # Calling method get_media_data and passing some meta data
-                yield scrapy.Request(userMediaLink + "&_signature=" + userMediaSignature.stdout.decode("utf-8"), callback=self.get_media_data, meta={'userID': userInfo['userID']})
-        except():
-            print("Not able the get info about this user:" + response.meta['userLink'])
+        # Generating user media singature using nodeJS browser.js script. Script is called using Naked lib
+        userMediaLink = "https://m.tiktok.com/api/item_list/?count=30&id=" + userInfo['userID'] + "&type=1&secUid=" + userInfo['userSecUid'] + "&maxCursor=0&minCursor=0&sourceType=8&appId=1233&region=CZ&language=cs&verifyFp="
+        userMediaSignature = muterun_js('D:\\NodeJS\\node_modules\\tiktok-signature\\browser.js', '"' + userMediaLink + '"')   # TODO get correct absolute path to the script
+        if userMediaSignature.exitcode == 0:
+            # Calling method get_media_data and passing some meta data
+            yield scrapy.Request(userMediaLink + "&_signature=" + userMediaSignature.stdout.decode("utf-8"), callback=self.get_media_data, meta={'userID': userInfo['userID']})
 
     # Method for getting user's media data
     def get_media_data(self, response):
@@ -117,9 +120,6 @@ class get_data_user(scrapy.Spider):
 
 
 # code for runnig spider straight from script and not with CLI
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'items.json'
-})
+process = CrawlerProcess(settings={})
 process.crawl(get_data_user)
 process.start()  # the script will block here until the crawling is finished
