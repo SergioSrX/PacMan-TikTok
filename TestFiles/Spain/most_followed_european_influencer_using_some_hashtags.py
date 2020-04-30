37720a5815cbbf18d30dc20eefa9bdbea6bb7d6b
msagaseta <msagaseta@github.com>
2020-04-30 14:54:24 +0200

    Update most_followed_european_influencer_using_some_hashtags.py

--- TestFiles/Spain/most_followed_european_influencer_using_some_hashtags.py
+++ TestFiles/Spain/most_followed_european_influencer_using_some_hashtags.py
@@ -8,7 +8,7 @@ db = client.userData
 collection = db.userData
 hashtags = ["europe","heritage"]
 europeans_countries = [ "Spain", "Germany","United Kingdom","United Kingdom",'Russian Federation' ]
-#Most followed euroean TikTok user using #europe hashtag
+#Most followed European TikTok user using each hashtag
 for hashtag in hashtags:
 	user = collection.find({'userRegion': { "$in": europeans_countries},"mediaData.hashtagsData.title":hashtag}, {'_id': False,'userName':1,'userRegion':1,"userStats":1,"mediaData.$":1}).sort("userStats.userFollowers",pymongo.DESCENDING).limit(1)	
 	data = pd.DataFrame(user,index=None)
